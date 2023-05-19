0d81812971bd365337c43252b867bd413974f252
Dustin Pyle <dooptydoo90x.dev@gmail.com>
2023-05-19 16:23:18 -0500

    attempted to make Ada work? succeeded a little

--- runtime/queries/ada/highlights.scm
+++ runtime/queries/ada/highlights.scm
@@ -176,5 +176,5 @@
 ;; Highlight errors in red. This is not very useful in practice, as text will
 ;; be highlighted as user types, and the error could be elsewhere in the code.
 ;; This also requires defining    :hi @error guifg=Red    for instance.
-(ERROR) @error
+;(ERROR) @error
 
